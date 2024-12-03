ca7fb37e9822f574781ca6c84ee6239a5c7d8919
iamthekidyouknowwhatitmean <truewarrior228@gmail.com>
2024-12-03 21:48:52 +0300

    Add files via upload
    мой шаблон

---
+++ local/templates/main/header.php
@@ -0,0 +1,20 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<!DOCTYPE html>
+<html lang="ru">
+<head><title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="<?=SITE_TEMPLATE_PATH?>/assets/images/favicon.604825ed.ico" type="image/x-icon">
+    <link href="<?=SITE_TEMPLATE_PATH?>/assets/css/common.css" rel="stylesheet">
+	<?$APPLICATION->ShowHead();?>
+	<title><?$APPLICATION->ShowTitle()?></title>
+</head>
+<body>
+<div id="#panel">
+	<?php $APPLICATION ->ShowPanel();?>
+	
+</div>
+
+</body>
+</html>
+
